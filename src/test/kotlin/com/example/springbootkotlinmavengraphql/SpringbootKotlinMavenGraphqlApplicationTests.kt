72351e72d8808f04181dd3ca7e50ca3b6cc36f6a
gajkumar <gajkumar@expediagroup.com>
2023-07-24 20:42:16 +0530

    first commit

---
+++ src/test/kotlin/com/example/springbootkotlinmavengraphql/SpringbootKotlinMavenGraphqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootkotlinmavengraphql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinMavenGraphqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
